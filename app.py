7efde2eda4b69852b86d772a50f06447a4e2ac6c
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-09-17 17:32:23 -0700

    Added ability to get top 10

--- app.py
+++ app.py
@@ -8,22 +8,33 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract query term from url
-    "q": q = {
-    "key": C8UDZFA60WIT
-    "limit": 10
+    q = request.args.get("search")
+    url = 'https://api.tenor.com/v1/search?'
+
+    params = {
+        "q": q,
+        "key": 'C8UDZFA60WIT',
+        "limit": 10,
+        "media_filter": "minimal"
     }
-    request.args.get("search")
+
 
     # TODO: Make 'params' dict with query term and API key
 
     # TODO: Make an API call to Tenor using the 'requests' library
-    r = requests.get(
-    "https://api.tenor.com/v1/search
+    r = requests.get(url, params)
     # TODO: Get the first 10 results from the search results
-
+    if r.status_code == 200:
+        results = json.loads(r.content)['results']
+    else:
+        results = None
     # TODO: Render the 'index.html' template, passing the gifs as a named parameter
+    print(results)
 
-    return render_template("index.html")
+    return render_template(
+    "index.html.j2",
+    gifs=results
+    )
 
 if __name__ == '__main__':
     app.run(debug=True)
